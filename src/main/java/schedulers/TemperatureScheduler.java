2964c0050138dc8e30032af188446d4c523fbc40
Niklas <niklas.linz@hotmail.com>
2018-05-01 11:54:51 +0200

    Upload

---
+++ src/main/java/schedulers/TemperatureScheduler.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2017. Niklas Linz - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the LGPLv3 license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the LGPLv3 license with
+ * this file. If not, please write to: niklas.linz@enigmar.de
+ *
+ */
+
+package schedulers;
+
+import de.linzn.leegianOS.internal.ifaces.IScheduler;
+
+public class TemperatureScheduler implements IScheduler {
+
+    @Override
+    public void loadScheduler() {
+
+    }
+}

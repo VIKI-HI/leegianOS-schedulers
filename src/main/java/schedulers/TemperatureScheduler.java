cd665d1d29992cc430404cde43a8c35d148fc74e
Niklas <niklas.linz@hotmail.com>
2018-05-02 20:20:47 +0200

    Update

--- src/main/java/schedulers/TemperatureScheduler.java
+++ src/main/java/schedulers/TemperatureScheduler.java
@@ -15,10 +15,14 @@ import de.linzn.leegianOS.LeegianOSApp;
 import de.linzn.leegianOS.internal.ifaces.IScheduler;
 import de.linzn.leegianOS.internal.lifeObjects.SchedulerSkillClient;
 import de.linzn.leegianOS.internal.lifeObjects.SubSkill;
+import de.linzn.whatsappApi.WhatsappClient;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import skills.ComputerTemplate;
+import skills.WhatsappTemplate;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -26,6 +30,9 @@ import java.util.concurrent.TimeUnit;
 public class TemperatureScheduler implements IScheduler {
 
     private boolean terminated = false;
+    private LinkedList<JSONObject> data = new LinkedList<>();
+    private int[] heat = {30, 70, 80};
+    private String testNumber = "";
 
     @Override
     public void loadScheduler() {
@@ -42,6 +49,50 @@ public class TemperatureScheduler implements IScheduler {
         }, 10, 20, TimeUnit.SECONDS);
     }
 
+    @Override
+    public void loopback() {
+        checkValid();
+        if (!data.isEmpty()) {
+            JSONObject jsonObject = data.removeFirst();
+            JSONArray jsonArray = jsonObject.getJSONObject("dataValues").getJSONArray("temperatures");
+            double hotCore = -1;
+            for (int i = 0; i < jsonArray.length(); i++){
+                double value = jsonArray.getDouble(i);
+                if (hotCore < value){
+                    hotCore = value;
+                }
+            }
+            if (hotCore >= heat[0]){
+
+                SchedulerSkillClient schedulerSkillClient = (SchedulerSkillClient) LeegianOSApp.leegianOSAppInstance.skillClientList.get(schedulerUUID());
+                WhatsappTemplate whatsappTemplate = new WhatsappTemplate();
+                Map map = new HashMap();
+                SubSkill subSkill = new SubSkill(0, null, null, null, null, null, map);
+                whatsappTemplate.setEnv(schedulerSkillClient, null, subSkill);
+
+                map.put("loginPhone", "xxx");
+                map.put("loginPassphrase", "xxx");
+                map.put("receiverPhone", "xxx");
+
+                if (hotCore >= heat[1]){
+                    if (hotCore >= heat[2]){
+                        map.put("message", "CRITICAL: Core temp over " + hotCore + "°C");
+                    } else {
+                        map.put("message", "WARNING: Core over " + hotCore + "°C");
+                    }
+                } else {
+                    map.put("message", "INFO: Core over " + hotCore + "°C");
+                }
+                whatsappTemplate.sendPhoneMessage();
+            }
+        }
+    }
+
+    @Override
+    public void addAnswerData(JSONObject json) {
+        this.data.add(json);
+    }
+
 
     @Override
     public void terminateScheduler() {
@@ -53,19 +104,12 @@ public class TemperatureScheduler implements IScheduler {
         return UUID.fromString("8d6c0b8f-9930-4f47-b516-45f116e161e4");
     }
 
-    @Override
-    public void addAnswerData(JSONObject data) {
-        boolean needResponse = data.getJSONObject("dataValues").getBoolean("needResponse");
-        String notificationText = data.getJSONObject("textValues").getString("notificationText");
-        System.out.println("Answer:" +  notificationText);
-    }
 
     private void checkValid() {
         if (terminated) {
             LeegianOSApp.logger(this.getClass().getSimpleName() + "->" + "terminate old scheduler ");
             Thread.currentThread().interrupt();
             Thread.currentThread().stop();
-            return;
         }
     }
 }

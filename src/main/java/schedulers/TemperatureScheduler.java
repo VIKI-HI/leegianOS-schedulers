91a03f7034f4f74fd28b2bcb772f6a8eec0b41b2
Niklas <niklas.linz@hotmail.com>
2018-05-03 17:42:50 +0200

    Update new scheduler

--- src/main/java/schedulers/TemperatureScheduler.java
+++ src/main/java/schedulers/TemperatureScheduler.java
@@ -14,6 +14,7 @@ import de.linzn.leegianOS.LeegianOSApp;
 import de.linzn.leegianOS.internal.ifaces.IScheduler;
 import de.linzn.leegianOS.internal.lifeObjects.SchedulerSkillClient;
 import de.linzn.leegianOS.internal.lifeObjects.SubSkill;
+import de.linzn.leegianOS.internal.lifeObjects.TimeData;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import skills.ComputerTemplate;
@@ -27,15 +28,12 @@ import java.util.concurrent.TimeUnit;
 
 public class TemperatureScheduler implements IScheduler {
 
-    private boolean terminated = false;
+    private boolean alive = false;
     private LinkedList<JSONObject> data = new LinkedList<>();
     private int[] heat = {65, 70, 80};
-    private String testNumber = "";
 
     @Override
-    public void loadScheduler() {
-        LeegianOSApp.leegianOSAppInstance.heartbeat.runRepeatTaskAsynchronous(() -> {
-            checkValid();
+    public void scheduler() {
             SchedulerSkillClient schedulerSkillClient = (SchedulerSkillClient) LeegianOSApp.leegianOSAppInstance.skillClientList.get(schedulerUUID());
             ComputerTemplate computerTemplate = new ComputerTemplate();
             Map map = new HashMap();
@@ -44,12 +42,10 @@ public class TemperatureScheduler implements IScheduler {
 
             map.put("hostName", "10.40.0.20");
             computerTemplate.getSystemTemperature();
-        }, 10, 20, TimeUnit.SECONDS);
     }
 
     @Override
     public void loopback() {
-        checkValid();
         if (!data.isEmpty()) {
             JSONObject jsonObject = data.removeFirst();
             JSONArray jsonArray = jsonObject.getJSONObject("dataValues").getJSONArray("temperatures");
@@ -68,9 +64,9 @@ public class TemperatureScheduler implements IScheduler {
                 SubSkill subSkill = new SubSkill(0, null, null, null, null, null, map);
                 whatsappTemplate.setEnv(schedulerSkillClient, null, subSkill);
 
-                map.put("loginPhone", "4915732538949");
-                map.put("loginPassphrase", "e/wN4yZaVy0ZGBFjXm4o17Pz0QU=");
-                map.put("receiverPhone", "4917634459132");
+                map.put("loginPhone", "x");
+                map.put("loginPassphrase", "x");
+                map.put("receiverPhone", "x");
 
                 if (hotCore >= heat[1]){
                     if (hotCore >= heat[2]){
@@ -87,27 +83,34 @@ public class TemperatureScheduler implements IScheduler {
     }
 
     @Override
+    public TimeData scheduler_timer() {
+        return new TimeData(10, 20, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public TimeData loopBack_timer() {
+        return new TimeData(100, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
     public void addAnswerData(JSONObject json) {
         this.data.add(json);
     }
 
 
     @Override
-    public void terminateScheduler() {
-        terminated = true;
-    }
-
-    @Override
     public UUID schedulerUUID() {
         return UUID.fromString("8d6c0b8f-9930-4f47-b516-45f116e161e4");
     }
 
-
-    private void checkValid() {
-        if (terminated) {
-            LeegianOSApp.logger(this.getClass().getSimpleName() + "->" + "terminate old scheduler ");
-            Thread.currentThread().interrupt();
-            Thread.currentThread().stop();
-        }
+    @Override
+    public boolean is_alive() {
+        return this.alive;
     }
+
+    @Override
+    public void set_alive(boolean b) {
+        this.alive = b;
+    }
+
 }
